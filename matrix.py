eba296a217c62b01c3c833dc1fead8a288f7e924
Walter Mitty <32843491+skate1512@users.noreply.github.com>
2020-02-22 22:17:05 +0530

    Update matrix.py

--- matrix.py
+++ matrix.py
@@ -1,3 +1,8 @@
+"""
+You are provided with the number of rows (R) and columns (C). 
+Your task is to generate the matrix having R rows and C columns such that 
+all the numbers are in increasing order starting from 1 in row wise manner.
+"""
 x,y=map(int,input().split())
 count=1
 R=[]
